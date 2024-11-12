71064b2cbafd084f78e815524587225ffb645866
Weedanta <baguswedanta17@gmail.com>
2024-11-12 22:03:55 +0700

    Data dan Model

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,9 @@ Route::get('/', function () {
 Route::get('/praktikum-laravel', function () {
     return view('praktikum2.index');
 });
+
+Route::get('events/{location}/{name}', [App\Http\Controllers\EventsController::class, 'show']);
+
+Route::get('/events', [App\Http\Controllers\EventsController::class, 'index']);
+
+
