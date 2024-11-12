54154f44687d0182c11cbf8d0dfac3f18b7a296b
Weedanta <baguswedanta17@gmail.com>
2024-11-12 23:03:28 +0700

    Authentication dengan Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,47 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
 
 Route::get('/praktikum-laravel', function () {
     return view('praktikum2.index');
 });
 
-Route::get('events/{location}/{name}', [App\Http\Controllers\EventsController::class, 'show']);
+Route::get('events', [EventsController::class, 'index']);
+Route::get('events/{location}/{name}', [EventsController::class, 'show']);
 
-Route::get('/events', [App\Http\Controllers\EventsController::class, 'index']);
+Route::get('events/validate', [App\Http\Controllers\EventsController::class, 'showValidateForm'])->middleware(['auth'])->name('validateform.event');
+Route::post('events/validate', [App\Http\Controllers\EventsController::class, 'validateForm'])->middleware(['auth'])->name('validate.event');
 
-Route::get('events/validate',[App\Http\Controllers\EventsController::class, 'showValidateForm'])->name('validateform.event');
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::post('events/validate',[App\Http\Controllers\EventsController::class, 'validateForm'])->name('validate.event');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
